f9e7df5204c881901014206fc6597389a7f47865
bczoma <balazs.czoma@solace.com>
2018-06-27 15:24:28 -0400

    Test permission fix

--- scripts/prepareProject.sh
+++ scripts/prepareProject.sh
@@ -46,12 +46,14 @@ if [[ "`oc get projects | grep tiller`" ]]; then
   echo "Tiller project detected, adding access to the ${1} project..."
   oc policy add-role-to-user edit system:serviceaccount:$TILLER:tiller
   oadm policy add-cluster-role-to-user storage-admin system:serviceaccount:$TILLER:tiller
+  oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$TILLER:tiller
   echo
 fi
 
 # Configure the required OpenShift Policies and SCC privileges for the operation of the Solace message router software
 echo "Granting the ${1} project policies and SCC privileges for correct operation..."
 oc policy add-role-to-user edit system:serviceaccount:$PROJECT:default
+oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$PROJECT:default
 oadm policy add-scc-to-user privileged system:serviceaccount:$PROJECT:default
 oadm policy add-scc-to-user anyuid system:serviceaccount:$PROJECT:default
 
